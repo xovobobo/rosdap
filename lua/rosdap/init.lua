7c99e1f530b7239a6252dc2d4b35999ded321a7b
bobo <pakhomovda@a-r-s.dev>
2023-08-05 12:50:57 +0500

    update

--- lua/rosdap/init.lua
+++ lua/rosdap/init.lua
@@ -1,12 +1,23 @@
-print("hello world")
+vim.g.rosdap_dir = vim.fn.stdpath('data') .. '/lazy/rosdap/'
+
+local getpython_output = function (path)
+    return vim.fn.system("python3 " .. path)
+end
+
+
 Get_ros_config = function()
     print("called")
-    local file_path = vim.fn.getcwd() .. "/ros_getconfig.py"
-    if io.open(file_path, "r") ~= nil then
-        print("readable")
+    local python_get_cfg_path = vim.g.rosdap_dir .. "ros_get_config.py"
+    local found = false
+    if io.open(python_get_cfg_path, "r") ~= nil then
+        
+        local result = getpython_output(python_get_cfg_path)
+
     else
-        print(file_path, "was not found")
+        print(python_get_cfg_path, "was not found")
     end
 end,
+
+
 
 vim.api.nvim_set_keymap('n', '<Leader>tc', ':lua Get_ros_config()<CR>', { noremap = true, silent = true, desc="Get ros configuration" })
