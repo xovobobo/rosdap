88c7365712a80a6f19b03d0566170229ed89044f
pakhomovda <pakhomovda@a-r-s.dev>
2023-08-05 16:39:18 +0500

    launch nodes

--- lua/rosdap/init.lua
+++ lua/rosdap/init.lua
@@ -13,16 +13,26 @@ local check_file = function (path)
     end
 end
 
+local launch_vimspector = function(configurations)
+    local json_obj = vim.fn.json_decode(configurations)
+    
+    for key, cfg in ipairs(json_obj['python']) do
+        print(key, vim.inspect(cfg))
+        vim.fn['vimspector#LaunchWithConfigurations'](cfg)
+    end
+end
+
 Get_ros_config = function()
     local python_get_cfg_path = vim.g.rosdap_dir .. "ros_get_config.py"
     local roslaunch_file = vim.fn.getcwd() .. "/.roslaunch"
 
     if (check_file(python_get_cfg_path) == true) and (check_file(roslaunch_file) == true)then
         local configurations = getpython_output(python_get_cfg_path, roslaunch_file)
-        print(configurations)
+        launch_vimspector(configurations)
     end
 end,
 
 
+    
 
 vim.api.nvim_set_keymap('n', '<Leader>tc', ':lua Get_ros_config()<CR>', { noremap = true, silent = true, desc="Get ros configuration" })
